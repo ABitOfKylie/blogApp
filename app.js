103cc61dabc704d4ffa02c7704438122568cfac8
Kylie <kyliecalif@gmail.com>
2017-11-14 19:48:59 -0800

    partials added, new, create and show routes and views in place

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ var bodyParser  = require("body-parser"),
     app         = express();
  
 mongoose.connect("mongodb://localhost/restful_blog_app");
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
 
 // Mongoose/Model Config.
 var blogSchema = new mongoose.Schema({
@@ -18,18 +19,18 @@ var blogSchema = new mongoose.Schema({
 });
 var Blog = mongoose.model("Blog", blogSchema);
 
-Blog.create({
-    title: "First Blog",
-    image: "https://9to5mac.files.wordpress.com/2017/11/iphonexcamerareivew_mann01-2.jpg?quality=82&w=2500#038;strip=all&w=1600",
-    body: "This is my first blog post",
-
-});
+// Blog.create({
+//     title: "Third Blog",
+//     image: "http://humboldtmuseum.ca/sites/default/files/events_pic/intro-oil-painting.jpg",
+//     body: "Not for the timid at heart, handle the brushes and oils with caution!!! Topping soufflé wafer I love wafer I love fruitcake macaroon cotton candy. Bonbon carrot cake sugar plum icing caramels tootsie roll. Dragée I love caramels I love gummies oat cake jujubes I love icing. I love liquorice I love lemon drops."
+// });
 
 // Routes
 app.get("/", function(req, res){
     res.redirect("/blogs");
 });
 
+//Index Route
 app.get("/blogs", function(req, res){
     Blog.find({}, function(err, blogs){
         if (err){
@@ -39,11 +40,36 @@ app.get("/blogs", function(req, res){
         }
     });
 });
+// New Route
+app.get("/blogs/new", function (req, res){
+    res.render("new");
+});
+//Create Route
+app.post("/blogs", function(req, res){
+    Blog.create(req.body.blog, function(err, bloginfo){
+        if(err){
+            res.render("new");
+        }else{
+            res.redirect("/blogs");
+        }
+    });
+});
+
+//SHOW ROUTE
+app.get("/blogs/:id", function(req, res){
+   Blog.findById(req.params.id, function(err, foundBlog){
+       if(err){
+           res.redirect("/blogs");
+       }else{
+           res.render("show", {blog: foundBlog});
+       }
+   });
+});
+
+// by using req.body.blog -- creates an object 
 
 
-
-
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Our blog server is running");
 });
     
