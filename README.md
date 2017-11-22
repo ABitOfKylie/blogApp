406eba57e0376e305cac59846f802346ee7571a8
Kylie <kyliecalif@gmail.com>
2017-11-22 12:42:53 -0800

    added notes, cleaned up comments, good to go

--- README.md
+++ README.md
@@ -1,26 +1,32 @@
 # Blog App based on Colt Steele's Udemy Tutorial
-Semantic-ui, ejs, express, mongoose and node.js
+
+Express, Mongo, mongoose & node.js - using Semantic-ui & ejs
+Following strict RESTful programming
+
+This app uses all 7 RESTful Routes:
+Index, New, Create, Show, Edit, Update and Destroy
+
+This app allows the user to view all blog posts, add a new blog post with image, edit it, and delete it. 
+
+Notes:
+In app.js
+Create & Update posts
+For the sake of 'practice' using req.body.blog -- to create a separate object within the req.body.object, helpful for larger projects. Say, the app has thousands of publishing, reactivated posts and comments all with a very long list of key values, I could have separate req.body.publishing, req.body.posts etc and it would cut down the processing time - it wouldn't have to comb through the whole req.body 
+Admittedly, req.body by itself would make for prettier code and more suitable for this tiny size project.
+
+Semantic Ui / Styling:
+I found Semantic ui to be a bit inflexible when it came to customization. 
+The docs are good if you wish to simply copy/paste the code and no need to customize. 
+The Semantic UI community is small. 
+Very limited selection of icons - but they do have them.
+Played around with the Grid System - Semantic grid = 16. Ehh. 
+As with most 'elements' in Semantic ui, I failed to see an advantage in using Semantic instead of other frameworks. 
+
+EJS - 
+Straightforward, a snap to learn and more than sufficient for a small app. 
+
+Express-Sanitizer was a nice find. Allows users to use markup language in the text-area/input fields, example add a <p> but strips unsafe tags and attributes from html
 
 
-Current dilemma:
 
-show.ejs
 
-ui icon buttons 'group'
-within - have an animated button inside an <a> tag to redirect to edit page -- works
-the 'normal' form delete button works - deletes the post 
-however --- trying to use an animated button with the form -- 
-
-So the button group now contains 4 btns.
-Edit, Trash, Delete, Home
-The 'normal' Delete button will be removed once the Trash button starts working.
-
-------
-
-Styling:
-Navbar -- change color to #AFCECF enlarge font-size
-segment - new page: #7eb1b3
-softer white - #f0ffff
-
-Limited icon choice with Semantic.
-Considering adding fontawesome to the mix. 
--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var bodyParser  = require("body-parser"),
+var bodyParser = require("body-parser"),
     expressSanitizer = require("express-sanitizer"),
     methodOverride = require("method-override"),
-    mongoose    = require("mongoose"),
-    ejs         = require("ejs"),
-    express     = require("express"),
-    app         = express();
+    mongoose = require("mongoose"),
+    ejs = require("ejs"),
+    express = require("express"),
+    app = express();
 
 mongoose.connect("mongodb://localhost/restful_blog_app");
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -20,7 +20,7 @@ var blogSchema = new mongoose.Schema({
     title: String,
     image: String,
     body: String,
-    created: {type: Date, default: Date.now}
+    created: { type: Date, default: Date.now }
 });
 var Blog = mongoose.model("Blog", blogSchema);
 
@@ -31,97 +31,90 @@ var Blog = mongoose.model("Blog", blogSchema);
 // });
 
 // Routes
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
     res.redirect("/blogs");
 });
 
-app.get("/restful", function(req,res){
+app.get("/restful", function(req, res) {
     res.render("restful");
 });
 
 //Index Route
-app.get("/blogs", function(req, res){
-    Blog.find({}, function(err, blogs){
-        if (err){
+app.get("/blogs", function(req, res) {
+    Blog.find({}, function(err, blogs) {
+        if (err) {
             console.log("oops");
-        } else{
-            res.render("index", {blogs: blogs});
+        } else {
+            res.render("index", { blogs: blogs });
         }
     });
 });
 // New Route
-app.get("/blogs/new", function (req, res){
+app.get("/blogs/new", function(req, res) {
     res.render("new");
 });
 //Create Route
-app.post("/blogs", function(req, res){
-    Blog.create(req.body.blog, function(err, bloginfo){
-        if(err){
+app.post("/blogs", function(req, res) {
+    Blog.create(req.body.blog, function(err, bloginfo) {
+        if (err) {
             res.render("new");
-        }else{
+        } else {
             res.redirect("/blogs");
         }
     });
 });
 
 //SHOW ROUTE
-app.get("/blogs/:id", function(req, res){
-   Blog.findById(req.params.id, function(err, foundBlog){
-       if(err){
-           res.redirect("/blogs");
-       }else{
-           res.render("show", {blog: foundBlog});
-       }
-   });
-});
-
-// by using req.body.blog -- creates an object 
-
-//EDIT ROUTE
-app.get("/blogs/:id/edit", function(req, res){
-    Blog.findById(req.params.id, function(err, foundBlog){
-        if(err){
+app.get("/blogs/:id", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if (err) {
             res.redirect("/blogs");
         } else {
-            res.render("edit", {blog: foundBlog});
+            res.render("show", { blog: foundBlog });
+        }
+    });
+});
+
+//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", { blog: foundBlog });
         }
     });
 });
 
 //UPDATE ROUTE
-app.put("/blogs/:id", function(req, res){
+app.put("/blogs/:id", function(req, res) {
     req.body.blog.body = req.sanitize(req.body.blog.body); // could use middleware to avoid repetition in routes 
-    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
-        if(err){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
+        if (err) {
             res.redirect("/blogs");
-        }else{
+        } else {
             res.redirect("/blogs/" + req.params.id);
         }
     });
-    
+
 });
 
 // DELETE ROUTE
-app.delete("/blogs/:id", function(req, res){
- Blog.findByIdAndRemove(req.params.id, function(err){
-     if(err){
-         res.redirect("/blogs/req.params.id");
-     }else{
-         res.redirect("/blogs");
-     }
- });
-    
-});
+app.delete("/blogs/:id", function(req, res) {
+    Blog.findByIdAndRemove(req.params.id, function(err) {
+        if (err) {
+            res.redirect("/blogs/req.params.id");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
 
+});
 
 // note: This doesn't have to be a delete request in order to delete something.
 // I could make it a app.get("/blogs/:id/delete"),
 // following RESTful route conventions - it needs to be a delete request.
 
-
-
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function() {
     console.log("Z blog server iz running");
 });
-
-
